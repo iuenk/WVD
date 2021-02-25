4db4560dc17438126280c53fd2ddfd8d6a1d36a6
Ivo Uenk <mail@udirection.com>
2021-02-25 22:02:47 +0100

    testen

---
+++ New-AzKeyVault.ps1
@@ -0,0 +1,27 @@
+$Customer = "UCORP"
+$TenantId = "a2de9bda-ffed-4527-96d7-d6f3ac10da8c"
+$SubscriptionId = "438eedbe-4df3-42b6-9bd2-5b7f8a069f4b"
+$ResourceGroupName = "Ucorp-Vault-RG"
+
+$domainadminuser = ConvertTo-SecureString "iuenk-a@ucorp.nl" -AsPlainText -Force
+$domainadminpassword = ConvertTo-SecureString "Zwemmen-IvoUenk1!" -AsPlainText -Force
+$domainname = ConvertTo-SecureString "ucorp.local" -AsPlainText -Force
+$domainoupath = ConvertTo-SecureString "OU=WVD,OU=Ucorp,DC=ucorp,DC=local" -AsPlainText -Force
+
+Connect-AzAccount -Tenant $TenantId -SubscriptionId $SubscriptionId
+
+$RGCheck = Get-AzResourceGroup | ?{$_.ResourceGroupName -eq $ResourceGroupName}
+if($RGCheck -eq $null){
+New-AzResourceGroup -name "Ucorp-Vault-RG" -Location "westeurope"
+}
+Else{
+    write-host "$ResourceGroupName already exists"
+}
+
+New-AzKeyVault -Name "Ucorp-WVD-KV" -ResourceGroupName $ResourceGroupName -Location "westeurope"
+Set-AzKeyVaultAccessPolicy -VaultName "Ucorp-WVD-KV" -UserPrincipalName "iuenk-a@ucorp.nl" -PermissionsToSecrets get,set,delete,list
+
+Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainadminuser" -SecretValue $domainadminuser
+Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainadminpassword" -SecretValue $domainadminpassword
+Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainname" -SecretValue $domainname
+Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainoupath" -SecretValue $domainoupath
