499632c327c7dffeb6374d39e1c189ca20399588
Ivo Uenk <mail@udirection.com>
2021-03-01 19:00:14 +0100

    s

--- New-AzKeyVault.ps1
+++ New-AzKeyVault.ps1
@@ -1,7 +1,8 @@
 $Customer = "UCORP"
 $TenantId = "a2de9bda-ffed-4527-96d7-d6f3ac10da8c"
 $SubscriptionId = "438eedbe-4df3-42b6-9bd2-5b7f8a069f4b"
-$ResourceGroupName = "Ucorp-Vault-RG"
+$ResourceGroupNameWVD = "$Customer-WVD-RG"
+$ResourceGroupNameVault = "$Customer-Vault-RG"
 
 $domainadminuser = ConvertTo-SecureString "iuenk-a@ucorp.nl" -AsPlainText -Force
 $domainadminpassword = ConvertTo-SecureString "<wachtwoord>" -AsPlainText -Force
@@ -10,15 +11,19 @@ $domainoupath = ConvertTo-SecureString "OU=WVD,OU=Ucorp,DC=ucorp,DC=local" -AsPl
 
 Connect-AzAccount -Tenant $TenantId -SubscriptionId $SubscriptionId
 
-$RGCheck = Get-AzResourceGroup | ?{$_.ResourceGroupName -eq $ResourceGroupName}
-if($RGCheck -eq $null){
-New-AzResourceGroup -name "Ucorp-Vault-RG" -Location "westeurope"
-}
-Else{
-    write-host "$ResourceGroupName already exists"
+#Create resourcegroup WVD
+$RGCheckWVD = Get-AzResourceGroup | ?{$_.ResourceGroupName -eq $ResourceGroupNameWVD}
+if($RGCheckWVD -eq $null){
+New-AzResourceGroup -name $ResourceGroupNameWVD -Location "westeurope"
 }
 
-New-AzKeyVault -Name "Ucorp-WVD-KV" -ResourceGroupName $ResourceGroupName -Location "westeurope"
+#Create resourcegroup Vault
+$RGCheckVault = Get-AzResourceGroup | ?{$_.ResourceGroupName -eq $ResourceGroupNameVault}
+if($RGCheckVault -eq $null){
+New-AzResourceGroup -name $ResourceGroupNameVault -Location "westeurope"
+}
+
+New-AzKeyVault -Name "Ucorp-WVD-KV" -ResourceGroupName $ResourceGroupNameVault -Location "westeurope"
 Set-AzKeyVaultAccessPolicy -VaultName "Ucorp-WVD-KV" -UserPrincipalName "iuenk-a@ucorp.nl" -PermissionsToSecrets get,set,delete,list
 Set-AzKeyVaultAccessPolicy -VaultName "Ucorp-WVD-KV" -ObjectId "f7f33d28-5b73-43c3-a7b5-655a7c307221" -PermissionsToSecrets get,set,delete,list
 
@@ -26,3 +31,9 @@ Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainadminuser" -SecretVa
 Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainadminpassword" -SecretValue $domainadminpassword
 Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainname" -SecretValue $domainname
 Set-AzKeyVaultSecret -VaultName "Ucorp-WVD-KV" -Name "domainoupath" -SecretValue $domainoupath
+
+#Create storage account (wel met ARM template doen denk ik)
+
+#Create Log analytics workspace
+
+#Create automation account met patch deployment schedule)
