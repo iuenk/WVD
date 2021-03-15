1a15d2e5d384a8ce1ed7a778ff493d2e56b23902
Ivo Uenk <mail@udirection.com>
2021-03-15 16:26:35 +0100

    x

--- PSScripts/1-Join-AzStorageAccountForAuth.ps1
+++ PSScripts/1-Join-AzStorageAccountForAuth.ps1
@@ -1,7 +1,7 @@
 #Define parameters
 $TenantId = "a2de9bda-ffed-4527-96d7-d6f3ac10da8c"
 $SubscriptionId = "438eedbe-4df3-42b6-9bd2-5b7f8a069f4b"
-$ResourceGroupName = "ucorp-storage-rg"
+$ResourceGroup = "ucorp-storage-rg"
 $StorageAccountName = "ucorpwvdstorage"
 $ServicePrincipalName = "githubactionazure"
 $servicePrincipalApplicationID = (Get-AzADServicePrincipal | Where-Object{$_.DisplayName -eq $ServicePrincipalName} | select -ExpandProperty Id)
@@ -37,10 +37,16 @@ Connect-AzAccount -ServicePrincipal -Credential $ServicePrincipalCreds  -Tenant 
 Select-AzSubscription -SubscriptionId $SubscriptionId 
 
 # Register the target storage account with your active directory environment under the target OU (for example: specify the OU with Name as "UserAccounts" or DistinguishedName as "OU=UserAccounts,DC=CONTOSO,DC=COM").
-Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -DomainAccountType "ComputerAccount"
+Join-AzStorageAccountForAuth -ResourceGroupName $ResourceGroup -StorageAccountName $StorageAccountName -DomainAccountType "ComputerAccount"
 
-#Run the command below if you want to enable AES 256 authentication. If you plan to use RC4, you can skip this step.
-#Update-AzStorageAccountAuthForAES256 -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName
+$fslogixOffice = "fslogixoffice"
+$fslogixProfiles = "fslogixprofiles"
 
-#You can run the Debug-AzStorageAccountAuth cmdlet to conduct a set of basic checks on your AD configuration with the logged on AD user. This cmdlet is supported on AzFilesHybrid v0.1.2+ version. For more details on the checks performed in this cmdlet, see Azure Files Windows troubleshooting guide.
-#Debug-AzStorageAccountAuth -StorageAccountName $StorageAccountName -ResourceGroupName $ResourceGroupName -Verbose
+# rechten nog zetten op de shares
+$SecurityGroupID = (Get-AzADGroup -DisplayName $SecurityGroup).id
+$fslogixOfficeId = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.storage/storageAccounts/$storageAccountName/fileServices/default/fileshares/$fslogixOffice"
+$fslogixProfilesId = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroup/providers/Microsoft.storage/storageAccounts/$storageAccountName/fileServices/default/fileshares/$fslogixProfiles"
+
+# To give individual accounts access to the file share (Kerberos), enable identity-based authentication for the storage account
+New-AzRoleAssignment -ObjectID $SecurityGroupID -RoleDefinitionName "storage File Data SMB Share Contributor" -Scope $fslogixOfficeId
+New-AzRoleAssignment -ObjectID $SecurityGroupID -RoleDefinitionName "storage File Data SMB Share Contributor" -Scope $fslogixProfilesId
