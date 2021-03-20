40cd51719adbe029a34f98f3ea8962bd2cdc4f4d
Ivo Uenk <mail@udirection.com>
2021-03-20 16:43:53 +0100

    s

---
+++ PSScripts/Start-Ucorp-WVD.ps1
@@ -0,0 +1,29 @@
+Import-Module Az.Accounts
+Import-Module Az.Automation
+Import-Module Az.Compute
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName       
+    "Logging in to Azure..."
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+#Start VM instances in a scale set
+Start-AzVmss -ResourceGroupName "ucorp-wvd-rg" -VMScaleSetName "Ucorp-WVD" -InstanceId "1"
