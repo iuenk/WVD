24dd10378713f2e322b75c8d2fd5ece80996304c
Ivo Uenk <mail@udirection.com>
2021-04-28 13:25:32 +0200

    s

---
+++ PSScripts/Install_FSlogix.ps1
@@ -0,0 +1,24 @@
+$path ="c:\Ucorp"
+$ErrorActionPreference = 'SilentlyContinue'
+
+$fsLogixURL="https://aka.ms/fslogix_download"
+$installerFile="fslogix_download.zip"
+
+Try
+{
+    $path ="c:\Ucorp"
+    mkdir $path -ErrorAction SilentlyContinue
+    Invoke-WebRequest $fsLogixURL -OutFile $path\$installerFile
+    Expand-Archive $path\$installerFile -DestinationPath $path\fsLogix\extract
+    Start-Process -FilePath $path\fsLogix\extract\x64\Release\FSLogixAppsSetup.exe -Args "/install /quiet /norestart" -Wait
+    Write-Host "Fslogix Install Succeeded"
+    
+}
+Catch
+{
+    Write-Host "Fslogix Install Failed"
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage
+    $FailedItem = $_.Exception.ItemName
+    Write-Host $FailedItem
+} 
