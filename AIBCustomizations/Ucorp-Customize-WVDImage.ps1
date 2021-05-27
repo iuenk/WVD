b94aa709911086c31c8aaad6eabaf55005c51f84
Ivo Uenk <mail@udirection.com>
2021-05-27 22:57:04 +0200

    s

--- AIBCustomizations/Ucorp-Customize-WVDImage.ps1
+++ AIBCustomizations/Ucorp-Customize-WVDImage.ps1
@@ -6,30 +6,28 @@
 .NOTES
     Author: Ivo Uenk
     Version: 0.1
-
-    Research Links: 
 #>
 
 ############################################## Create Temporary Installation folder (for downloaded resources) ##############################################
 New-Item -Path 'C:\Install' -ItemType Directory -Force | Out-Null
 
-#Datum/Tijd/Taal OS
-NL taalinstellen (zie https://docs.microsoft.com/nl-nl/azure/virtual-desktop/language-packs)
-Herstart nodig
-Pas tijdzone aan naar UTC +1
 
-Ga naar Configuratiescherm, klik en regio, dan Land/regio en tablad Beheer: Instellingen kopieren. Selecteer Aanmeldingsscherm en systeemaccounts en Nieuwe gebruikersaccounts.
+############################################## Datum/Tijd/Taal OS ############################################## 
+#NL taalinstellen (zie https://docs.microsoft.com/nl-nl/azure/virtual-desktop/language-packs en https://mscloud.be/azure/powershell/configure-regional-settings-and-windows-locales-on-azure-virtual-machines/)
+#Herstart nodig
+#Pas tijdzone aan naar UTC +1
+#Ga naar Configuratiescherm, klik en regio, dan Land/regio en tablad Beheer: Instellingen kopieren. Selecteer Aanmeldingsscherm en systeemaccounts en Nieuwe gebruikersaccounts.
+#Zie Set-RegionalSettings.ps1
 
 ############################################## Disable Store auto update ##############################################
-Schtasks /Change /Tn "\Microsoft\Windows\WindowsUpdate\Automatic app update" /Disable
+#Schtasks /Change /Tn "\Microsoft\Windows\WindowsUpdate\Automatic app update" /Disable (is niet aanwezig)
 Schtasks /Change /Tn "\Microsoft\Windows\WindowsUpdate\Scheduled Start" /Disable
+
 
 ############################################## Configure OneDrive for All Users ##############################################
 # Uninstall Onedrive
 taskkill.exe /F /IM "OneDrive.exe"
 Write-Output "Remove OneDrive"
-if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
-    & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
 }
 if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
     & "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
@@ -38,8 +36,6 @@ if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
 # Install OneDrive (check if OneDrive link is still valid)
 Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/p/?LinkID=844652&clcid=0x413&culture=nl-nl&country=NL' -OutFile 'c:\Install\OneDriveSetup.exe'
 Invoke-Expression -Command 'C:\Install\OneDriveSetup.exe /allusers'
-
-# Wait till installation of OneDrive is completed
 Start-Sleep -Seconds 20
 
 
@@ -47,30 +43,26 @@ Start-Sleep -Seconds 20
 reg add "HKLM\SOFTWARE\Microsoft\Teams\" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
 Invoke-WebRequest -Uri 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true' -OutFile 'c:\Install\Teams_windows_x64.msi'
 Invoke-Expression -Command 'C:\Install\Teams_windows_x64.msi OPTIONS="noAutoStart=true" ALLUSERS=1 ALLUSER=1'
-
-# Wait till installation of Teams WebSocket Optimizations client is completed
 Start-Sleep -Seconds 20
 
 # Turn off auto start Teams
+#HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run\com.squirrel.Teams.Teams
 
-Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vc_redist.x64.exe' -OutFile 'c:\Install\vc_redist.x64.exe'
-Invoke-Expression -Command 'C:\Install\vc_redist.x64.exe /quiet /norestart'
+# Install Teams WebSocket Optimizations client
+Invoke-WebRequest -Uri 'https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt' -OutFile 'c:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi'
+Invoke-Expression -Command 'C:\Install\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi /silent' 
+Start-Sleep -Seconds 5
 
-# Wait till installation of Teams WebSocket Optimizations client is completed
-Start-Sleep -Seconds 20
 
 ############################################## Install M365 NL ##############################################
 Invoke-WebRequest -Uri 'https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=languagepack&language=nl-nl&platform=x64&source=O16LAP&version=O16GA' -OutFile 'c:\Install\OfficeSetup.exe'
 Invoke-Expression -Command 'C:\Install\OfficeSetup.exe'
+Start-Sleep -Seconds 300
 
-Start-Sleep -Seconds 180
 
 ############################################## Add MSIX app attach certificate ##############################################
-Invoke-WebRequest -Uri 'https://ucorpwvdstd.blob.core.windows.net/wvdfilerepo/MSIXAppAttach.pfx?sp=r&st=2021-05-26T08:48:09Z&se=2023-05-26T16:48:09Z&spr=https&sv=2020-02-10&sr=b&sig=vs8O3TDPwthTVrn2CaZINgraNiYoZUPP1lIloXLOvQY%3D' -OutFile 'c:\Install\MSIXAppAttach.pfx'
-$mypwd = Get-Credential -UserName 'Enter password below'
-Import-PfxCertificate -FilePath 'C:\Install\MSIXAppAttach.pfx' -CertStoreLocation 'Cert:\LocalMachine\TrustedPeople' -Password $mypwd.Password -Exportable
-
-
+Invoke-WebRequest -Uri 'https://gsvwvdstd.blob.core.windows.net/gsvwvdrepo/GSV-MSIX.pfx?sp=r&st=2021-05-27T20:35:26Z&se=2025-05-28T04:35:26Z&spr=https&sv=2020-02-10&sr=b&sig=ov5DcGkwTmkAdSt2b8W77Ne5LCd1IJP5rxbquexcLdU%3D' -OutFile 'c:\Install\GSV-MSIX.pfx'
+Import-PfxCertificate -FilePath 'C:\Install\GSV-MSIX.pfx' -CertStoreLocation 'Cert:\LocalMachine\TrustedPeople' -Password (ConvertTo-SecureString -String 'Solvinity@GSV' -AsPlainText -Force) -Exportable
 
 #maakt het mounten van images mogelijk SeManageVolumePrivilege en geeft de tools om bestanden op te vragen van Azure file share onder system
 #Kopieer de PSTools https://docs.microsoft.com/en-us/sysinternals/downloads/psexec naar %Windir%\System32
@@ -81,77 +73,3 @@ Import-PfxCertificate -FilePath 'C:\Install\MSIXAppAttach.pfx' -CertStoreLocatio
 #Open CMD als administrator en voer het volgende uit:
 #psexec /s cmd
 #setPrivGpsvc
-
-
-# SIG # Begin signature block
-# MIINHAYJKoZIhvcNAQcCoIINDTCCDQkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUQUqfjLZyyTMB1rjICtSSedxX
-# MzagggpeMIIFJjCCBA6gAwIBAgIQCyXBE0rAWScxh3bGfykLTjANBgkqhkiG9w0B
-# AQsFADByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
-# VQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFz
-# c3VyZWQgSUQgQ29kZSBTaWduaW5nIENBMB4XDTIwMDgxMDAwMDAwMFoXDTIzMDgx
-# NTEyMDAwMFowYzELMAkGA1UEBhMCTkwxDzANBgNVBAcTBkxlbW1lcjEVMBMGA1UE
-# ChMMY29nbml0aW9uIElUMRUwEwYDVQQLEwxDb2RlIFNpZ25pbmcxFTATBgNVBAMT
-# DGNvZ25pdGlvbiBJVDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAL2y
-# YRbztz9wTtatpSJ5NMD0JZtDPhuFqAVTjoGc1jvn68M41zlGgi8fVvEccaH3nDTT
-# 6T8edgFuEbsZVHZGmY109zHOPwXX+Zvp3T+Hk2Ys8Liwwirr6xw9dlneBu85j8gd
-# Mamz+mNjzpyBg1eVlD7cV1JAL3oAXgONRiebdpD6DPvd3melPmeg84Un3VV6+W8M
-# 8Y0Pec+TbxIda18Lr4DqnIl0a/Suk8kQ2DzZXDXoK+MCfA6zsqyEOSY5yI5OwdU0
-# 93LC2PHFEKEkIogBlCiD0UQDbamPdu7wZnTAHPTDfifdMhCPLBA0y4pj4jm6ggFE
-# 3ZuQMR/yU8JXSwy72ZECAwEAAaOCAcUwggHBMB8GA1UdIwQYMBaAFFrEuXsqCqOl
-# 6nEDwGD5LfZldQ5YMB0GA1UdDgQWBBR2SeoVDh3RxGqV5iamn/FFU6J65zAOBgNV
-# HQ8BAf8EBAMCB4AwEwYDVR0lBAwwCgYIKwYBBQUHAwMwdwYDVR0fBHAwbjA1oDOg
-# MYYvaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJlZC1jcy1nMS5j
-# cmwwNaAzoDGGL2h0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQt
-# Y3MtZzEuY3JsMEwGA1UdIARFMEMwNwYJYIZIAYb9bAMBMCowKAYIKwYBBQUHAgEW
-# HGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCAYGZ4EMAQQBMIGEBggrBgEF
-# BQcBAQR4MHYwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBO
-# BggrBgEFBQcwAoZCaHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
-# U0hBMkFzc3VyZWRJRENvZGVTaWduaW5nQ0EuY3J0MAwGA1UdEwEB/wQCMAAwDQYJ
-# KoZIhvcNAQELBQADggEBAL4Zda674x5WLL8B059a9cxnUIC05LcjD/3hkCLZgbMa
-# krDrfsjNpA+KpMiTv2TW5pDRCXGJirJO27XRTojr2F8+gJAyIB+8ZLiyKmy3IcCV
-# DXjjb6i/4TiGbDmGL3Ctl5pmWRpksnr3TKSMyxz2OogLS6w9pgRdA1hgJSfZMV+a
-# KRrd4iW5YWKIwFZlYDeQqpBBtQ6ujzgQ/04FcmjyOlNch4hofJVLauzkSb1Tnzt1
-# 6TyT2pJ9BzasoOlxYEFhn0ikXndlKVBb7gpFInqSf5DJtaVRIXojj0eqN6LZroUz
-# 62m2YeR29uC06xcdF7fjo+YKxe+kdApdPfX0Nx9Moc8wggUwMIIEGKADAgECAhAE
-# CRgbX9W7ZnVTQ7VvlVAIMA0GCSqGSIb3DQEBCwUAMGUxCzAJBgNVBAYTAlVTMRUw
-# EwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20x
-# JDAiBgNVBAMTG0RpZ2lDZXJ0IEFzc3VyZWQgSUQgUm9vdCBDQTAeFw0xMzEwMjIx
-# MjAwMDBaFw0yODEwMjIxMjAwMDBaMHIxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxE
-# aWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xMTAvBgNVBAMT
-# KERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25pbmcgQ0EwggEiMA0G
-# CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQD407Mcfw4Rr2d3B9MLMUkZz9D7RZmx
-# OttE9X/lqJ3bMtdx6nadBS63j/qSQ8Cl+YnUNxnXtqrwnIal2CWsDnkoOn7p0WfT
-# xvspJ8fTeyOU5JEjlpB3gvmhhCNmElQzUHSxKCa7JGnCwlLyFGeKiUXULaGj6Ygs
-# IJWuHEqHCN8M9eJNYBi+qsSyrnAxZjNxPqxwoqvOf+l8y5Kh5TsxHM/q8grkV7tK
-# tel05iv+bMt+dDk2DZDv5LVOpKnqagqrhPOsZ061xPeM0SAlI+sIZD5SlsHyDxL0
-# xY4PwaLoLFH3c7y9hbFig3NBggfkOItqcyDQD2RzPJ6fpjOp/RnfJZPRAgMBAAGj
-# ggHNMIIByTASBgNVHRMBAf8ECDAGAQH/AgEAMA4GA1UdDwEB/wQEAwIBhjATBgNV
-# HSUEDDAKBggrBgEFBQcDAzB5BggrBgEFBQcBAQRtMGswJAYIKwYBBQUHMAGGGGh0
-# dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBDBggrBgEFBQcwAoY3aHR0cDovL2NhY2Vy
-# dHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNydDCBgQYD
-# VR0fBHoweDA6oDigNoY0aHR0cDovL2NybDQuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
-# QXNzdXJlZElEUm9vdENBLmNybDA6oDigNoY0aHR0cDovL2NybDMuZGlnaWNlcnQu
-# Y29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNybDBPBgNVHSAESDBGMDgGCmCG
-# SAGG/WwAAgQwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29t
-# L0NQUzAKBghghkgBhv1sAzAdBgNVHQ4EFgQUWsS5eyoKo6XqcQPAYPkt9mV1Dlgw
-# HwYDVR0jBBgwFoAUReuir/SSy4IxLVGLp6chnfNtyA8wDQYJKoZIhvcNAQELBQAD
-# ggEBAD7sDVoks/Mi0RXILHwlKXaoHV0cLToaxO8wYdd+C2D9wz0PxK+L/e8q3yBV
-# N7Dh9tGSdQ9RtG6ljlriXiSBThCk7j9xjmMOE0ut119EefM2FAaK95xGTlz/kLEb
-# Bw6RFfu6r7VRwo0kriTGxycqoSkoGjpxKAI8LpGjwCUR4pwUR6F6aGivm6dcIFzZ
-# cbEMj7uo+MUSaJ/PQMtARKUT8OZkDCUIQjKyNookAv4vcn4c10lFluhZHen6dGRr
-# sutmQ9qzsIzV6Q3d9gEgzpkxYz0IGhizgZtPxpMQBvwHgfqL2vmCSfdibqFT+hKU
-# GIUukpHqaGxEMrJmoecYpJpkUe8xggIoMIICJAIBATCBhjByMQswCQYDVQQGEwJV
-# UzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQu
-# Y29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29kZSBTaWdu
-# aW5nIENBAhALJcETSsBZJzGHdsZ/KQtOMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
-# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
-# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRBfeNiVPF4
-# YnkL9x6aDUXLudc/wjANBgkqhkiG9w0BAQEFAASCAQCd52MMT/Dqo3th0g+7gaEX
-# QYwJBt1j2Xu7aM5r8FYfPEWgk/hxzgT6p+BkEqxT4dsobEhOf5ULd8ULFz1Rtb40
-# +ZyQN+6yLX2nyn57zuvr44tpebKEKp7pmolyXwjj4Wzn1GcsmFiwDfZ0KbmeKHHc
-# plcZw1tHK9Vq0DzvIlDzEePFEiCTCxCkWfg6A4VbtICGL1d0QknFOho8Mu/exGHA
-# 1aq3xpURXniXzrG/qDaYurRTH4QW+tmsdYEpdf5tzrna9gaL5qryI1EKCJaM74BV
-# T594ljdUBHjGDI5GcVRlj+EGdwmABYyvd1CwvUq5MoR6kz06N89ANVIpH708xsxb
-# SIG # End signature block
